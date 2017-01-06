6dbf3809fdc860c97ef5b6ce664bc8af551920e7
hollythedev <papiniu@outlook.com>
2017-01-06 15:05:22 -0800

    Created react component

--- client/main.js
+++ client/main.js
@@ -1,2 +1,17 @@
 //Any JS in here is automatically ran 
 
+
+//Imports the React library here
+
+import React from 'react';
+
+//Create a component
+const App = () => {
+    return(
+        <div>
+        React App #2
+        </div>
+    );
+};
+
+//Render this component to the screen
