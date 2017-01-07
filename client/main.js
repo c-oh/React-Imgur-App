4894d177364d8802bbcb880a6e67a5c4f817110c
hollythedev <papiniu@outlook.com>
2017-01-06 21:25:41 -0800

    importent component from react and allows us to extend it from the "class" App

--- client/main.js
+++ client/main.js
@@ -3,19 +3,21 @@
 
 //Imports the React library here
 
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 
 //Create a component
-const App = () => {
+class App extends Component {
+    render (){
     return(
         <div>
         <ImageList /> 
         </div>
     );
+    };
 };
 
 //Render this component to the screen
